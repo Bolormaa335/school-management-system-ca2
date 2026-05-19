65137a667f5243952061bc56edda5ed2c9d29f12
Bolormaa335 <2025335@student.cct.ie>
2026-05-19 09:54:39 +0100

    Added valid school roles and departments lists

---
+++ SchoolManagementSystem/src/main/java/CA_2/EmployeeManager.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Acer
+ */
+
+
+public class EmployeeManager {
+    public static String[] validRoles ={
+        "Principal",
+        "Assistant Principal",
+        "Head Teacher",
+        "Teacher",
+        "Teaching Assistant",
+        "Coordinator",
+        "Manager",
+        "Officer",
+        "Assistant"
+    };
+    // Valid school departments
+    public static String[] validDepartments = {
+        "IT Department",
+        "HR Office",
+        "Finance Office",
+        "Admissions Office",
+        "Marketing Office",
+        "Student Services",
+        "Operations Office",
+        "Accounts Office",
+        "IT Support"
+    };
+    
+
+
+    
+
