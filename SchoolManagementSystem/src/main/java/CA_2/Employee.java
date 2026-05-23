9856dfa4fca4f43249bd9dc998a63c2b4fe9bcd4
Bolormaa335 <2025335@student.cct.ie>
2026-05-23 08:45:54 +0100

    commented

--- SchoolManagementSystem/src/main/java/CA_2/Employee.java
+++ SchoolManagementSystem/src/main/java/CA_2/Employee.java
@@ -8,20 +8,28 @@ package CA_2;
  *
  * @author Acer
  */
-// Employee class stores one school staff record
+// Employee class stores one employee record
+// Each employee object contains employee name,
+// school role and department information
+
 public class Employee {
     
-    // Employee full name
+    //  Stores Employee full name
     private String name;
 
-    // Employee school role
+    // Stores Employee school role
     private String role;
 
-    // Employee department
+    //  Stores Employee department
     private String department;
 
-    // Constructor to create an employee object
+    // Constructor used to create a new employee object
+    // The constructor receives employee name,
+    // role and department as parameters
     public Employee(String name, String role, String department) {
+          
+        
+       // Assign parameter values to object variables
         this.name = name;
         this.role = role;
         this.department = department;
